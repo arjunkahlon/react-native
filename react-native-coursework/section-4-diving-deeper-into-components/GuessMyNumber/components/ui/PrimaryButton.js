377aed57da9614140bc65bfbbeb71a18154542d7
Arjun Kahlon <arjunskahlon@gmail.com>
2023-04-09 01:50:16 -0700

    Implemented functionality for generating new random guess after user hint
    Alert user if their hint is incorrect to ensure valid number range.

--- react-native-coursework/section-4-diving-deeper-into-components/GuessMyNumber/components/ui/PrimaryButton.js
+++ react-native-coursework/section-4-diving-deeper-into-components/GuessMyNumber/components/ui/PrimaryButton.js
@@ -3,7 +3,6 @@ import { View, Text, Pressable, StyleSheet } from "react-native";
 import Colors from "../../constants/colors";
 
 function PrimaryButton({children, onPress}) {
-
 	return (
 		<View style={styles.buttonOuterContainer}>
 			<Pressable
--- react-native-coursework/section-4-diving-deeper-into-components/GuessMyNumber/screens/GameScreen.js
+++ react-native-coursework/section-4-diving-deeper-into-components/GuessMyNumber/screens/GameScreen.js
@@ -1,15 +1,37 @@
 import { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Alert } from "react-native";
 
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
 import generateRandomBetween from "../utilities/generate-random-between";
 
-
+let minBoundary = 1;
+let maxBoundary = 100;
 
 function GameScreen({userNumber}) {
-  const initialGuess = generateRandomBetween(1, 100, userNumber)
+  const initialGuess = generateRandomBetween(minBoundary, maxBoundary, userNumber)
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  function nextGuessHandler(direction) {
+    if (
+        (direction === 'lower' && currentGuess < userNumber) ||
+        (direction === 'greater' && currentGuess > userNumber)
+    ) {
+      Alert.alert("Don't lie!", "You know that this is wrong...", [
+        { text: 'Sorry!', style: 'cancel' }
+      ]);
+      return;
+    }
+
+    if (direction === 'lower') {
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+    const newGuess = generateRandomBetween(minBoundary, maxBoundary, userNumber);
+    setCurrentGuess(newGuess);
+  };
 
   return (
     <View style={styles.screen}>
@@ -17,6 +39,10 @@ function GameScreen({userNumber}) {
       <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or Lower?</Text>
+        <View>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
+        </View>
       </View>
     </View>
   );
