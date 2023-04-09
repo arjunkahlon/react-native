ff5feff8bdd8d463637e2935203a26a8f1782a8a
Arjun Kahlon <arjunskahlon@gmail.com>
2023-04-09 00:04:14 -0700

    Updated App component to switch to Game Screen after number is picked

--- react-native-coursework/section-4-diving-deeper-into-components/GuessMyNumber/App.js
+++ react-native-coursework/section-4-diving-deeper-into-components/GuessMyNumber/App.js
@@ -1,8 +1,23 @@
-import StartGameScreen from './screens/StartGameScreen';
+import { useState } from 'react';
 import { StyleSheet, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>;
+
+  if (userNumber) {
+    screen = <GameScreen />;
+  }
+
   return (
     <LinearGradient 
       style={styles.rootScreen}
@@ -14,11 +29,12 @@ export default function App() {
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
-        <StartGameScreen />
+        {screen}
       </ImageBackground>
     </LinearGradient>
   );
 }
+
 
 const styles = StyleSheet.create({
   rootScreen: {
