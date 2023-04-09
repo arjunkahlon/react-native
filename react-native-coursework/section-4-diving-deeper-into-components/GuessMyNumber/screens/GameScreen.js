598940a985a73808206e4c0f0d4a2af6f07eadc6
Arjun Kahlon <arjunskahlon@gmail.com>
2023-04-09 03:30:59 -0700

    Implemented GameScreen icons with Ionicons provided by Expo

--- react-native-coursework/section-4-diving-deeper-into-components/GuessMyNumber/screens/GameScreen.js
+++ react-native-coursework/section-4-diving-deeper-into-components/GuessMyNumber/screens/GameScreen.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { View, Text, StyleSheet, Alert } from "react-native";
+import { Ionicons } from '@expo/vector-icons'
 
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
@@ -50,10 +51,14 @@ function GameScreen({ userNumber, onGameOver }) {
         <InstructionText style={styles.instructionText}>Higher or Lower?</InstructionText>
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
+              <Ionicons name="md-remove" size={24} color="white" />
+            </PrimaryButton>
           </View>
           <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
+              <Ionicons name="md-add" size={24} color="white" />
+            </PrimaryButton>
           </View>
         </View>
       </Card>
