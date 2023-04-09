ac3c3837bb04259307f502a6633948dbf9cd960e
Arjun Kahlon <arjunskahlon@gmail.com>
2023-04-09 03:46:41 -0700

    Implemented open-sans font with useFonts hook provided by Expo

--- react-native-coursework/section-4-diving-deeper-into-components/GuessMyNumber/components/ui/Title.js
+++ react-native-coursework/section-4-diving-deeper-into-components/GuessMyNumber/components/ui/Title.js
@@ -1,6 +1,5 @@
 import { StyleSheet, Text } from "react-native";
 
-import Colors from "../../constants/colors";
 
 function Title({children}) {
   return <Text style={styles.title}>{children}</Text>
@@ -11,8 +10,8 @@ export default Title;
 
 const styles = StyleSheet.create({
   title: {
+    fontFamily: 'open-sans-bold',
     fontSize: 18,
-    fontWeight: 'bold',
     color: 'white',
     textAlign: 'center',
     borderWidth: 2,
