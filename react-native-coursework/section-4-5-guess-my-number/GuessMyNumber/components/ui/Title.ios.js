9174f1b91c580db008a271e8a14d5967a03f1d1a
Arjun Kahlon <arjunskahlon@gmail.com>
2023-04-09 20:27:36 -0700

    Updated Colors and Title Component to platform specific files

---
+++ react-native-coursework/section-4-5-guess-my-number/GuessMyNumber/components/ui/Title.ios.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, Platform } from "react-native";
+
+
+function Title({children}) {
+  return <Text style={styles.title}>{children}</Text>
+
+}
+
+export default Title;
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 18,
+    color: 'white',
+    textAlign: 'center',
+    padding: 12,
+    maxWidth: '80%',
+    width: 300
+  }
+})
