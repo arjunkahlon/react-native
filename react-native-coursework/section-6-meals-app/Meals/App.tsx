0ea04d40ab696098a8c00dc6440e1823a33b140a
Arjun Kahlon <arjunskahlon@gmail.com>
2023-04-10 23:10:54 -0700

    Implemented Navigation in Application root
    Utilizes the Stack Navigator provided by
    react-navigation.

--- react-native-coursework/section-6-meals-app/Meals/App.tsx
+++ react-native-coursework/section-6-meals-app/Meals/App.tsx
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import CategoriesScreen from './screens/CategoriesScreen';
+
+type RootStackParamList = {
+  MealsCategories: undefined;
+};
+
+
+
+const RootStack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <>
-      <StatusBar style='light'/>
-      <CategoriesScreen />
+      <StatusBar style='dark'/>
+      <NavigationContainer>
+        <RootStack.Navigator>
+          <RootStack.Screen
+            name='MealsCategories'
+            component={CategoriesScreen}
+          />
+        </RootStack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
