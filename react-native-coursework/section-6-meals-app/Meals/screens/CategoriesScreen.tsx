374c3305408e4141bf08a3915e9703755b30d739
Arjun Kahlon <arjunskahlon@gmail.com>
2023-04-10 22:11:19 -0700

    Implemented layout/styling in CategoryGridTile Component.
    Implemented touchable feedback for iOS and Android. Added
    background color for the tile which is provided through props.

--- react-native-coursework/section-6-meals-app/Meals/screens/CategoriesScreen.tsx
+++ react-native-coursework/section-6-meals-app/Meals/screens/CategoriesScreen.tsx
@@ -6,11 +6,13 @@ import CategoryGridTitle from "../components/CategoryGridTile";
 
 function CategoriesScreen() {
   return <FlatList 
+          key='categories'
           data={CATEGORIES} 
           keyExtractor={(item) => item.id}
           renderItem={({ item }: ListRenderItemInfo<Category>) =>{
             return <CategoryGridTitle title={item.title} color={item.color}/>
           }}
+          numColumns={2}
         />
 }
 
