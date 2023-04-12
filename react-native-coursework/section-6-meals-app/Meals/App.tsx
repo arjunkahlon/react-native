ddb8fa6bbfd4b4d57c13bed96bfe1b12d108813f
Arjun Kahlon <arjunskahlon@gmail.com>
2023-04-12 11:54:48 -0700

    Implemented Drawer navigation to Meals App.
    Drawer allows user to navigate between meal
    categories and meal favorites. Favorites will
    be implemented in next app feature.

--- react-native-coursework/section-6-meals-app/Meals/App.tsx
+++ react-native-coursework/section-6-meals-app/Meals/App.tsx
@@ -3,13 +3,17 @@ import { StyleSheet } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { DrawerContent, createDrawerNavigator } from "@react-navigation/drawer";
+import "react-native-gesture-handler";
+import { Ionicons } from '@expo/vector-icons'
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
 
 type RootStackParamList = {
-  MealsCategories: undefined;
+  Drawer: undefined;
   MealsOverview: {categoryId: string};
   MealDetail: {mealId: string}
 };
@@ -17,6 +21,49 @@ type RootStackParamList = {
 
 
 const RootStack = createStackNavigator<RootStackParamList>();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#351401',
+        },
+        headerTintColor: 'white',
+        sceneContainerStyle: {
+          backgroundColor: 'black'
+        },
+        drawerContentStyle: {
+          backgroundColor: '#351401',
+        },
+        drawerInactiveTintColor: 'white',
+        drawerActiveTintColor: '#e4baa1',
+      }}
+    >
+      <Drawer.Screen
+        name='MealsCategories'
+        component={CategoriesScreen}
+        options={{
+          title: 'All Categories',
+          drawerIcon: ({color, size}) => (
+            <Ionicons name='list' color={color} size={size}  />
+          )
+        }}
+      />
+      <Drawer.Screen 
+      name='Favorites'
+      component={FavoritesScreen}
+      options={{
+        drawerIcon: ({color, size}) => (
+          <Ionicons name='star' color={color} size={size}  />
+        )
+      }}
+      />
+    </Drawer.Navigator>
+  )
+
+}
 
 export default function App() {
   return (
@@ -35,10 +82,11 @@ export default function App() {
           }}
         >
           <RootStack.Screen
-            name='MealsCategories'
-            component={CategoriesScreen}
+            name='Drawer'
+            component={DrawerNavigator}
             options={{
-              title: 'All Categories',
+              title: 'Categories',
+              headerShown: false
             }}
           />
            <RootStack.Screen
