6ff43447f5dcfa038d6b3fc687dca436e549c016
Arjun Kahlon <arjunskahlon@gmail.com>
2023-04-13 15:10:46 -0700

    Updated FavoritesScreen to display list of favorited meals

--- react-native-coursework/section-6-meals-app/Meals/screens/FavoritesScreen.tsx
+++ react-native-coursework/section-6-meals-app/Meals/screens/FavoritesScreen.tsx
@@ -1,24 +1,38 @@
+import { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native'
 
+import MealsList from '../components/MealsList/MealsList';
+import { FavoritesContext } from '../store/context/FavoritesContext';
+import { MEALS } from '../data/meal-data';
+
 function FavoritesScreen() {
-  return (
-          <View style={styles.screen}>
-            <Text style={styles.text}>Favorites</Text>
-            <Text style={styles.text}>This Favorites Screen will be implemented in next feature</Text>
-          </View>
-  )
+  const favoriteMealsContext = useContext(FavoritesContext);
+  const favoriteMeals = MEALS.filter(meal => favoriteMealsContext.ids.includes(meal.id));
+  
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite meals yet.</Text>
+      </View>
+    );
+  }
+
+  return <MealsList items={favoriteMeals} />;
+    
 }
 
 export default FavoritesScreen;
 
 const styles = StyleSheet.create({
-  screen: {
+  rootContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
   },
   text: {
-    color: 'white',
-    margin: 10
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white'
   }
+
 })
