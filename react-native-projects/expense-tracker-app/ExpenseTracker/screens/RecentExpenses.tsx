9893ba98345e421c78cfe9bc3e720e130fc7db41
Arjun Kahlon <arjunskahlon@gmail.com>
2023-04-30 21:40:21 -0700

    Implemented functionality for fetching expenses from Firebase

--- react-native-projects/expense-tracker-app/ExpenseTracker/screens/RecentExpenses.tsx
+++ react-native-projects/expense-tracker-app/ExpenseTracker/screens/RecentExpenses.tsx
@@ -1,11 +1,18 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 
 import { ExpensesContext } from '../store/ExpensesContext';
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 import { getDateMinusDays } from '../util/date';
+import { fetchExpenses } from '../util/http';
 
 function RecentExpenses() {
-  const expensesContext = useContext(ExpensesContext)
+  const expensesContext = useContext(ExpensesContext);
+
+  useEffect(() => {
+    async function getExpenses() {
+      const expenses = await fetchExpenses();
+    }
+  }, []);
 
   const recentExpenses = expensesContext.expenses.filter((expense) => {
     const today = new Date();
--- react-native-projects/expense-tracker-app/ExpenseTracker/util/http.ts
+++ react-native-projects/expense-tracker-app/ExpenseTracker/util/http.ts
@@ -1,10 +1,32 @@
 import axios from "axios";
 
-import { ExpenseData } from "../types/Expense";
+import { Expense, ExpenseData } from "../types/Expense";
+
+const BACKEND_URL = 'https://react-native-course-cee7f-default-rtdb.firebaseio.com'
 
 export function storeExpense(expenseData: ExpenseData) {
   axios.post(
-    'https://react-native-course-cee7f-default-rtdb.firebaseio.com/expenses.json',
+    BACKEND_URL + '/expenses.json',
     expenseData,
     );
+}
+
+export async function fetchExpenses() {  
+   const response = await axios.get(
+    BACKEND_URL + '/expenses.json'
+  );
+
+  const expenses: Expense[] = [];
+
+  for (const key in response.data) {
+    const expenseObj: Expense = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description
+    };
+    expenses.push(expenseObj);
+  }
+
+  return expenses;
 }
